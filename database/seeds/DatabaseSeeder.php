791b678a5d6db73a5bcb509fcd1fe0714ce17ad8
Tanvi Singh <ts377@njit.edu>
2018-05-04 19:25:52 -0400

    updated databaseSeeder.php

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,7 +13,7 @@ class DatabaseSeeder extends Seeder
     {
          $this->call(UsersTableSeeder::class);
          $this->call(QuestionsTableSeeder::class);
-        $this->call(AnswersTableSeeder::class);
+       // $this->call(AnswersTableSeeder::class);
 
 
     }
